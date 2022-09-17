7fa66c7ad9e71c2c700b7de11dea803e5587a585
Guillermo Jofre <jofregf@gmail.com>
2022-09-17 11:31:44 -0300

    Java Basico Ejercicio Tema 2

---
+++ src/com/campus/open/bootcamp/PrecioConIva.java
@@ -0,0 +1,17 @@
+package com.campus.open.bootcamp;
+
+public class PrecioConIva {
+
+    public static void main(String[] args) {
+
+        getPrecioTotal(100.00);
+    }
+
+    public static void getPrecioTotal(double precio) {
+        float iva = 21.00f;
+        double resultado = precio + (precio * (iva/100));
+        System.out.println("El precio con IVA incluido es " + resultado);
+    }
+
+
+}
